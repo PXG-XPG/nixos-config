1c2782643402ab2da7b4c585223e0ea93d9e1e0e
PXG-XPG <rum66627@gmail.com>
2026-05-10 15:28:03 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,7 @@
   imports = [ 
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
 
       # Include base configuration
       ./base
